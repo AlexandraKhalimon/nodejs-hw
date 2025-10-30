417b413ec7c747233fdf3f57654be57b884b4864
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-10-31 00:37:59 +0200

    add filter

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
   console.log(`Error:${err.message}`);
-  
+
   const isProd = process.env.NODE_ENV === "production";
   res.status(500).json({
     message: isProd
--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { model, Schema } from 'mongoose';
+import { TAGS } from '../constants/tags';
 
 const noteSchema = new Schema(
   {
@@ -13,7 +14,7 @@ const noteSchema = new Schema(
     },
     tag: {
       type: String,
-      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -22,5 +23,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ title: "text", content: "text" });
 
 export const Note = model('Note', noteSchema);
