c7a4f4ea996cec7ccfa7fb5094a3571e61a8ec46
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-11-01 00:31:04 +0200

    add authenticate and logout

--- src/models/note.js
+++ src/models/note.js
@@ -10,6 +10,7 @@ const noteSchema = new Schema(
     },
     content: {
       type: String,
+      default: '',
       trim: true,
     },
     tag: {
@@ -17,6 +18,11 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    }
   },
   {
     timestamps: true,
--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import {notesRoutes, authRoutes} from './routes/notesRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
 
