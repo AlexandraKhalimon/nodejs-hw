d6e7a3f7890e005523a23fa0402e96ecee7be23f
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-10-31 12:31:38 +0200

    add register

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+import { celebrate } from "celebrate";
+import { Router } from "express";
+import { registerUserSchema } from "../validations/authValidation";
+import { registerUser } from "../controllers/authController";
+
+const router = Router();
+
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -5,8 +5,9 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from './routes/notesRoutes.js';
+import {notesRoutes, authRoutes} from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,7 +15,9 @@ const PORT = process.env.PORT;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
